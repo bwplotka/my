e29e9e53f50766ac30a9d4c7c25fa97f4dde92b4
Bartek Plotka <bwplotka@gmail.com>
2019-03-10 16:58:59 +0000

    Additional improvements.
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>

---
+++ web/assets/js/main.js
@@ -0,0 +1,44 @@
+jQuery(function($) {
+
+    'use strict';
+    var _Blog = window._Blog || {};
+
+    _Blog.prettify = function() {
+        $('pre').addClass('prettyprint linenums').attr('style', 'overflow:auto;');
+        window.prettyPrint && prettyPrint();
+    };
+
+    _Blog.externalUrl = function() {
+        $.expr[':'].external = function(obj) {
+            return !obj.href.match(/^mailto\:/) &&
+                (obj.hostname != location.hostname);
+        };
+        $('a:external').addClass('external');
+        $(".external").attr('target', '_blank');
+
+    }
+
+    _Blog.toggleTheme = function() {
+        const currentTheme = window.localStorage && window.localStorage.getItem('theme')
+        const isLight = currentTheme === 'light'
+        $('body').toggleClass('dark-theme', !isLight)
+        $('.theme-switch').on('click', () => {
+            $('body').toggleClass('dark-theme')
+            window.localStorage &&
+                window.localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark' : 'light', )
+        })
+    }
+
+    _Blog.toggleMobileMenu = function() {
+        $('.menu-toggle').on('click', () => {
+            $('.menu-toggle').toggleClass('active')
+            $('#mobile-menu').toggleClass('active')
+        })
+    }
+
+    $(document).ready(function() {
+        _Blog.prettify()
+        _Blog.toggleTheme()
+        _Blog.toggleMobileMenu()
+    });
+});
