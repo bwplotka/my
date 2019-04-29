1d66372055a1bc77537609c0bf70c1bf479fd332
Bartek Plotka <bwplotka@gmail.com>
2019-04-29 01:25:53 +0100

    Added YOLO comments section.
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>

---
+++ web/assets/js/gh-comments.js
@@ -0,0 +1,73 @@
+jQuery(function($) {
+    const repo = "my";
+    const owner = "bwplotka";
+
+    'use strict';
+    var _Blog = window._Blog || {};
+
+    _Blog.renderComment = function(issue) {
+        // TODO(bwplotka): Use some templating instead?
+        var comment = $("<div/>");
+        comment.addClass("post-gh-comment");
+
+        // TODO(bwplotka): Limit number of lines?
+        var body = $("<p/>");
+        body.text(issue.body);
+
+        var src = $("<a/>");
+        src.text("Source");
+        src.attr("href", issue.html_url);
+
+        var author = $("<a/>");
+        author.text(issue.user.login);
+        author.attr("href", issue.user.html_url);
+
+        comment.append(body);
+
+        var commentFooter = $("<div/>");
+        commentFooter.addClass("post-gh-comment-footer");
+        commentFooter.append("<time datetime=" + issue.updated_at + ">"+issue.updated_at +"</time>");
+        commentFooter.append(" | ", src);
+        commentFooter.append(" | <span>Author: ");
+        commentFooter.append(author);
+        commentFooter.append(" | Responses: ", issue.comments, "</span>");
+
+        comment.append(commentFooter);
+
+        $('.post-gh-comments').append(comment);
+        _Blog.issues[issue.id] = true
+    };
+    _Blog.loadPostComments = function() {
+        $('.post-gh-comments-loading').text("Loading...");
+        _Blog.octokit.issues.listForRepo({
+            owner: owner,
+            repo: repo,
+            state: 'all',
+            labels: _Blog.name + ",comment",
+            sort: 'created',
+            direction: 'asc'
+        }).then(({data, headers, status}) => {
+            if (status !== 200) {
+                console.log("failed to load issues:", status, data)
+                return
+            }
+            console.log(_Blog.name);
+            console.log(data);
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].id in _Blog.issues) {
+                    continue
+                }
+                _Blog.renderComment(data[i])
+            }
+            $('.post-gh-comments-loading').text("");
+        })
+    };
+
+    $(document).ready(function() {
+        _Blog.name = $('article').attr('id');
+        _Blog.octokit = new Octokit();
+        _Blog.issues={};
+        _Blog.loadPostComments();
+        setInterval(_Blog.loadPostComments, 10000);
+    });
+});
